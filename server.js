90405c52bc52023eb82c545fa843ef0bc0a4e444
Axel Smeets <murlocbrand+git@gmail.com>
2014-09-05 02:03:42 +0200

    content

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+	fs = require('fs')
+
+http.createServer( function (req, res) {
+    fs.readFile('index.html', function (err, data) {
+
+        res.writeHead(200, {
+        	'Content-Type': 'text/html',
+        	'Content-Length' : data.length
+        })
+
+        res.write(data)
+        res.end()
+    })
+}).listen(8080)
